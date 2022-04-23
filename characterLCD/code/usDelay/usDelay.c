62cc91deb8b8bb53985cdc14188ac20d8405a7e5
Taeho-Cho <57129682+Taeho-Cho@users.noreply.github.com>
2022-04-23 12:16:09 +0900

    Update usDelay.c

--- characterLCD/code/usDelay/usDelay.c
+++ characterLCD/code/usDelay/usDelay.c
@@ -22,7 +22,7 @@ bool usDelayInit()
 		htim6.Instance = TIM6;
 		htim6.Init.Prescaler = 80 - 1;
 		htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-		htim6.Init.Period = 1000 - 1;
+		htim6.Init.Period = 65535;
 		htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 		if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
 		{
